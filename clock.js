ed9844e3784b99535553dfc949193f15e6af065d
Prasid <prasidupadhya@gmail.com>
2026-09-10 00:11:22 +0200

    feat: add bilingual library and localized footer

--- clock.js
+++ clock.js
@@ -1,11 +1,18 @@
 const footerClock = document.querySelector("[data-live-clock]");
-const footerClockFormatter = new Intl.DateTimeFormat(undefined, {
-  hour: "2-digit",
-  minute: "2-digit",
-  day: "2-digit",
-  month: "long",
-  year: "numeric"
-});
+const CLOCK_LOCALES = Object.freeze({ en: "en-GB", es: "es-ES" });
+let clockLanguage = document.documentElement.lang === "es" ? "es" : "en";
+
+function createClockFormatter(language) {
+  return new Intl.DateTimeFormat(CLOCK_LOCALES[language], {
+    hour: "2-digit",
+    minute: "2-digit",
+    day: "2-digit",
+    month: "long",
+    year: "numeric"
+  });
+}
+
+let footerClockFormatter = createClockFormatter(clockLanguage);
 
 function updateFooterClock() {
   const now = new Date();
@@ -15,3 +22,9 @@ function updateFooterClock() {
 
 updateFooterClock();
 window.setInterval(updateFooterClock, 1000);
+
+window.addEventListener("bookshelf:languagechange", (event) => {
+  clockLanguage = event.detail.language;
+  footerClockFormatter = createClockFormatter(clockLanguage);
+  updateFooterClock();
+});
